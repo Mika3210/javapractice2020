7935cb742aeec9c06fd60f23e032ed7d779e68e4
Mika3210 <lizakurnosova@mail.ru>
2020-12-07 11:44:14 +0400

    Конец

--- src/ru/Mika3210/javapractice2020/pr29/Order.java
+++ src/ru/Mika3210/javapractice2020/pr29/Order.java
@@ -1,19 +1,13 @@
 package ru.Mika3210.javapractice2020.pr29;
 
-import java.util.HashMap;
-
 interface Order {
     boolean addPosition(Item a);
-    boolean addPosition(Item a, HashMap<String, Order> hashMap);
-    boolean addPosition(HashMap<String, Order> hashMap);
-    int delete(Item a);
-    int delete(Item a, HashMap<String, Order> hashMap);
+    String[] names();
     int count();
-    Item getCount(HashMap<String, Order> hashMap);
-    Item arrayCount();
-    Item arrayCountSorted();
+    int count(String itemName);
+    Item[] getItems();
+    boolean delete(String itemName);
+    int deleteAll(String itemName);
+    Item[] sorted();
     int cost();
-    int totalOrders(String name);
-    int total(Item a);
-    Item arrayCountNames();
 }
--- src/ru/Mika3210/javapractice2020/pr29/OrderAlreadyAddedException.java
+++ src/ru/Mika3210/javapractice2020/pr29/OrderAlreadyAddedException.java
@@ -8,7 +8,9 @@ public class OrderAlreadyAddedException extends Exception {
         return count==0;
     }
     public void addAfter(Item a, Item b){
+    }
 
+    public OrderAlreadyAddedException() {
     }
 
     public OrderAlreadyAddedException(InternetOrder<Item> position, int count) {
